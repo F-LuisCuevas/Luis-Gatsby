09457e6c028a7c1e619141e333aeffe25d1b1f42
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-02-17 17:56:38 -0700

    restyling about home

--- src/components/About.js
+++ src/components/About.js
@@ -11,28 +11,40 @@ export default function About() {
   return (
     <div className="container">
       <div className="my-5"></div>
-      <div className="display-3">
-        <span style={{ color: "white" }}>Hi!</span> <br />
+      <div className="row mb-n4">
+        <div className="col-md">
+          <div className="display-3">
+            <span style={{ color: "white" }}>Hi!</span> <br />
+          </div>
+          <div className="h1 code mt-4 mb-3" style={{ color: "#8589ef" }}>
+            const eliVillalejos <span style={{ color: "#ff79c6" }}>= </span>
+            <span style={{ color: "white" }}>{"{"}</span>
+          </div>
+          <div
+            className="h4 mx-5 my-4 text-justify info"
+            style={{ color: "#d3d3d3	" }}
+          >
+            occupation:{" "}
+            <span style={{ color: "#18cebb" }}>
+              "Full-stack web developer",
+            </span>
+            <br />
+            techStack:{" "}
+            <span style={{ color: "#18cebb" }}>
+              [React, JS, RoR, Ruby, Redux],
+            </span>
+            <br />
+            likes:{" "}
+            <span style={{ color: "#18cebb" }}>[Code, Dogs, Coffee]</span>
+            <br />
+            <div className="h1 code mt-2 mb-3">{"}"}</div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="hello-img"></div>
+        </div>
       </div>
-      <div className="h1 code mt-4 mb-3" style={{ color: "#8589ef" }}>
-        const eliVillalejos <span style={{ color: "#ff79c6" }}>= </span>
-        <span style={{ color: "white" }}>{"{"}</span>
-      </div>
-      <div
-        className="h4 mx-5 my-4 text-justify info"
-        style={{ color: "#d3d3d3	" }}
-      >
-        occupation:{" "}
-        <span style={{ color: "#18cebb" }}>"Full-stack web developer"</span>
-        <br />
-        techStack:{" "}
-        <span style={{ color: "#18cebb" }}>[React, JS, RoR, Ruby, Redux]</span>
-        <br />
-        likes: <span style={{ color: "#18cebb" }}>[Code, Dogs, Coffee]</span>
-        <br />
-      </div>
-      <div className="h1 code mt-2 mb-3">{"}"}</div>
-      <div className="h1 mt-5 mb-5 footer ">
+      <div className="h1 mt-5 mb-5 footer">
         <a className="mr-5 icon" href="https://twitter.com/miss_elliev">
           <Twitter />
         </a>
---
+++ src/components/gallery.js
@@ -0,0 +1,51 @@
+import React from "react"
+import "../style.css"
+import Navbar from "../components/navbar"
+import { graphql } from "gatsby"
+import Card from "../components/card"
+import { Helmet } from "react-helmet"
+
+const Gallery = ({ data }) => {
+  const projectList = data.allProjectsYaml.edges
+
+  return (
+    <div className="container-fluid">
+      <Navbar />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Projects | Eli Vilallejos</title>
+      </Helmet>
+      <div className="row">
+        {projectList.map(({ node }) => {
+          return (
+            <Card
+              cardTitle={node.title}
+              cardSubtitle={node.subtitle}
+              link={node.link}
+              key={node.id}
+              live={node.live}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Gallery
+
+export const query = graphql`
+  {
+    allProjectsYaml {
+      edges {
+        node {
+          title
+          subtitle
+          link
+          id
+          live
+        }
+      }
+    }
+  }
+`
