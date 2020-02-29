9854c215c5fb1a2aa7d1769df1a74bae7f6fcf21
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-02-28 17:30:28 -0700

    Updating config to display favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#282a36`,
         theme_color: `#282a36	`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-yaml`,
