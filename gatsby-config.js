55c7447550ed2dec5de136967de27cb59fb57262
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-03-02 17:55:31 -0700

    Adding gatsby Image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-netlify`,
     {
