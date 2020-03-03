250a80e6b12337a7995ab2bd6f41dadcf6f8041a
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-03-02 18:50:24 -0700

    Correcting filenames in imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "../style.css"
 import Navbar from "../components/Navbar"
-import About from "../components/about"
+import About from "../components/About"
 import { Helmet } from "react-helmet"
 
 function index() {
